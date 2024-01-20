c1543b20799d02f746d98fa20e2a6ebcd3509073
Андрей Сибен <siben.ae@dvfu.ru>
2024-01-20 17:57:21 +1000

    Добавлено основное приложение. Форматирование текста (шрифт), вставка изображений и гиперссылок. Сохранение/открытие файла

---
+++ TextProcessor/Exit.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TextProcessor
+{
+    /// <summary>
+    /// Логика взаимодействия для Exit.xaml
+    /// </summary>
+    public partial class Exit : Window
+    {
+        public string mWCurrentlyOpenFile;
+        public string mWRawRtf;
+        public RichTextBox mWRichTextBox;
+
+        public void getArguments(string currentlyOpenPath, string rawRtf)
+        {
+            mWCurrentlyOpenFile = currentlyOpenPath;
+            mWRawRtf = rawRtf;
+        }
+
+        public Exit()
+        {
+            InitializeComponent();
+        }
+
+        private void bExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void bSave_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MainWindow = new MainWindow();
+            if (mWCurrentlyOpenFile == null)
+            {
+                Nullable<bool> result = MainWindow.sfdSave.ShowDialog();
+                if (result == true)
+                {
+                    if (MainWindow.saveDocument(MainWindow.sfdSave.FileName, mWRichTextBox))
+                    {
+                        this.DialogResult = false;
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    MainWindow.saveDocument(MainWindow.sfdSave.FileName, mWRichTextBox);
+                    this.DialogResult = false;
+                }
+                catch (Exception ex)
+                {
+                    CustomMessageBox messageBox = new CustomMessageBox();
+                    messageBox.SetupMsgBox(ex.Message + "\nТекстовый процессор пытается продолжить работу.", "Ошибка!", this.FindResource("iconError"));
+                    messageBox.ShowDialog();
+                    throw;
+                }
+            }
+        }
+
+        private void bCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
